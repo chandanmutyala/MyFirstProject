4c8f69ef1901c36d930ca79e6f663d36c6c2d925
chandanmutyala <chandanmutyala@gmail.com>
2024-01-28 21:23:08 +0530

    initial

---
+++ profiles.js
@@ -0,0 +1,24 @@
+export const profiles = [
+    
+      {
+        "name": "Gopi Chandh",
+        "picture": "john_doe.jpg",
+        "experience": "5 years",
+        "worked_companies": ["ABC Tech", "XYZ Solutions"],
+        "expertise": ["JavaScript", "Web Development"]
+      },
+      {
+        "name": "Aparna",
+        "picture": "alice_smith.jpg",
+        "experience": "5 years",
+        "worked_companies": ["Data Insights Corp", "Tech Analytics Ltd"],
+        "expertise": ["React", "JavaScript", "Web Development"]
+      },
+      {
+        "name": "Sudheer",
+        "picture": "bob_johnson.jpg",
+        "experience": "3 years",
+        "worked_companies": ["Creative Designs Inc", "Pixel Perfect Studios"],
+        "expertise": ["User Interface Design","JavaScript", "Web Development"]
+      }
+    ]
